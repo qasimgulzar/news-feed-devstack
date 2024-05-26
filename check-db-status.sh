d5fbaf9c0b43fc25d34b95bb3e71cf1f22535758
qasimgulzar <qasim.khokhar52@gmail.com>
2024-05-27 00:26:29 +0500

    🎨set message color

--- check-db-status.sh
+++ check-db-status.sh
@@ -1,3 +1,4 @@
+GREEN='\033[0;32m'
 echo "${GREEN}Waiting for MySQL"
 until docker-compose exec -T db bash -c "mysql -uroot -se \"SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'root');\"" &> /dev/null
 	do
